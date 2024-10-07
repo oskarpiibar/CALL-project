4a97cd88c449608c21fd7f08020545faff54cc36
Oskar Piibar <oskar.piibar@gmail.com>
2024-10-07 05:28:49 +0200

    Venv alternations

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=c:\Users\oskar\OneDrive\Documents\Kool\UTwente\MOD9\Project\.venv
+set VIRTUAL_ENV=c:\Users\oskar\OneDrive\Documents\Kool\UTwente\MOD9\CALL-project\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
